b5e8cc7958f5a6aef8167dfd658d5c8744a19869
Albert <albertisntreal1180@gmail.com>
2025-08-19 12:08:44 -0600

    Fix service worker syntax and manifest icons

--- index.js
+++ index.js
@@ -107,8 +107,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+        secure: false, // Try setting this to false temporarily
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true
     }
 }));
 
